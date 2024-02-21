f7ee4737825f04d258d68becc74996532a116d18
HangyBoi <6avuha228@gmail.com>
2024-02-21 20:19:15 +0100

    Added my classes

---
+++ Lift Off/GXPEngine/Enemy.cs
@@ -0,0 +1,102 @@
+﻿using GXPEngine;
+using GXPEngine.Core;
+using System;
+using System.Drawing;
+
+public class Enemy : Entity
+{
+    public Player _player;
+
+    private bool isFollowingPlayer = false;
+    private float followDistance = 300f;
+
+    protected float playerX;
+    protected float playerY;
+    protected float enemyX;
+    protected float enemyY;
+
+
+    private float velX = 1.5f;
+    private float velY = 1;
+
+    public Sound buzzSound;
+
+    public Enemy(Player player)
+    {
+        _player = player;
+
+        buzzSound = new Sound("sounds/bee_buzz.mp3", false, false);
+
+        SetEntityIdleSprites("Assets/Enemy_Fly.png", 4, 1);
+        SetEntityRunSprites("Assets/Enemy_Fly.png", 4, 1);
+        SetEntityAttackSprites("Assets/Enemy_Attack.png", 4, 1);
+    }
+
+    public void Death()
+    {
+        buzzSound.Play();
+        Console.WriteLine("enemy died");
+        LateDestroy();
+    }
+
+    public void UpdateEnemy()
+    {
+        if (PlayerInRange())
+        {
+            isFollowingPlayer = true;
+            FollowPlayer();
+        }
+    }
+
+    private bool PlayerInRange()
+    {
+        float distanceToPlayer = DistanceToPlayer();
+        return distanceToPlayer <= followDistance;
+    }
+
+    private void FollowPlayer()
+    {
+        SetEntityState(EntityState.Run);
+        playerX = _player.x;
+        playerY = _player.y;
+
+        enemyX = x;
+        enemyY = y;
+
+        if (Mathf.Abs(playerX - enemyX) > 1)
+        {
+            if (playerX < enemyX)
+            {
+                Move(-velX, 0);
+                states[(int)EntityState.Run].Mirror(false, false);
+            }
+            else
+            {
+                Move(velX, 0);
+                states[(int)EntityState.Run].Mirror(true, false);
+            }
+        }
+
+        if (Mathf.Abs(playerY - enemyY) > 1)
+        {
+            if (playerY < enemyY)
+            {
+                Move(0, -velY);
+            }
+            else
+            {
+                Move(0, velY);
+            }
+        }
+    }
+
+    private float DistanceToPlayer()
+    {
+        playerX = _player.x;
+        playerY = _player.y;
+        enemyX = x;
+        enemyY = y;
+
+        return Mathf.Sqrt((playerX - enemyX) * (playerX - enemyX) + (playerY - enemyY) * (playerY - enemyY));
+    }
+}
---
+++ Lift Off/GXPEngine/Player.cs
@@ -0,0 +1,137 @@
+﻿using GXPEngine;
+using GXPEngine.Core;
+using System;
+using System.Drawing;
+
+
+public class Player : Entity
+{
+    protected float speed = 2f;
+
+    protected bool isFacingRight = true;
+
+    protected int _HP;
+
+    public bool isDead = false;
+    public bool isWin = false;
+
+    private Sound winMusic;
+
+    public Player()
+    {
+        SetEntityIdleSprites("Assets/B_witch_idle.png", 1, 6);
+        SetEntityRunSprites("Assets/B_witch_run.png", 1, 8);
+        SetEntityAttackSprites("Assets/B_witch_attack.png", 1, 9);
+
+        _HP = 3;
+
+        winMusic = new Sound("sounds/win.mp3", false, false);
+    }
+
+    private void HandleInput()
+    {
+        SetEntityState(EntityState.Idle);
+
+        if (Input.GetKey(Key.A))
+        {
+            states[(int)EntityState.Idle].Mirror(true, false);
+            isFacingRight = false;
+        }
+
+        if (Input.GetKey(Key.D))
+        {
+            states[(int)EntityState.Idle].Mirror(false, false);
+            isFacingRight = true;
+        }
+
+        /*
+
+        if (Input.GetKey(Key.W))
+        {
+            moving = true;
+            Move(0, -speed);
+            SetEntityState(EntityState.Run);
+            states[(int)EntityState.Run].Mirror(false, false);
+            isFacingRight = true;
+        }
+
+        if (Input.GetKey(Key.S))
+        {
+            moving = true;
+            Move(0, speed);
+            SetEntityState(EntityState.Run);
+            states[(int)EntityState.Run].Mirror(false, false);
+            isFacingRight = true;
+        }
+
+        if (!Input.GetKey(Key.A) && !Input.GetKey(Key.D))
+        {
+            moving = false;
+            SetEntityState(EntityState.Idle);
+            if (isFacingRight == false)
+            {
+                states[(int)EntityState.Idle].Mirror(true, false);
+            }
+            else
+            {
+                states[(int)EntityState.Idle].Mirror(false, false);
+            }
+        }
+
+        if (Input.GetKey(Key.X) && !moving)
+        {
+            SetEntityState(EntityState.Attack);
+        }
+        else if (Input.GetKeyUp(Key.X))
+        {
+            states[(int)EntityState.Attack].SetFrame(0);
+        }*/
+    }
+
+    public int GetScore()
+    {
+        return -1;
+    }
+
+    public int GetHP()
+    {
+        return _HP;
+    }
+
+    public void WinGame()
+    {
+        isWin = true;
+        winMusic.Play();
+    }
+
+    public void Death()
+    {
+        isDead = true;
+    }
+
+    public void UpdatePlayer()
+    {
+        HandleInput();
+    }
+}
+
+/*    private void CheckColissions()
+    {
+        GameObject[] collidngObjects = GetCollisions();
+        foreach (GameObject collidingObject in collidngObjects)
+        {
+            if (collidingObject is Enemy)
+            {
+                Enemy enemy = (Enemy)collidingObject;
+                Console.WriteLine("Enemy Touched!");
+                enemy.LateDestroy();
+                hitSound.Play();
+                _HP--;
+                if (_HP == 0)
+                {
+                    gameOver.Play();
+                    Death();
+                }
+            }
+        }
+    }*/
